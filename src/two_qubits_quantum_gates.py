743254ec775403431516164754d34490e89e41a8
noapr <pariente@campus.technion.ac.il>
2023-11-09 21:47:18 +0200

    add two qubits quantum gates: swap and cnot

---
+++ src/two_qubits_quantum_gates.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def swap_gate():
+    return np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
+
+
+def cnot_gate():
+    return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
